4c7331f3fce70612f82f2d40d8d0fba516b246c6
andy10134 <andy10134@hotmail.com>
2020-11-04 13:22:49 -0300

    cambios en constructor

--- proyecto/Models/classConexion.php
+++ proyecto/Models/classConexion.php
@@ -1,22 +1,22 @@
 <?php
     class Conexion{
         //atributos privados
-        private $host_name;
-        private $user;
-        private $pass;
+        private $dsn;
+        private $user; //root
+        private $pass; // ""
         private $conex;
 
         //constructor
-        function __construct($db_name){
-            $this->host_name = $db_name;
+        function __construct($db_name = "gim", $hostName = "localhost"){
+            $this->dsn = "/mysql:host=.$hostName.;dbname=.$db_name.";
             $this->user = 'root';
             $this->pass = null;
             $this->conexion();
         }
-
+        //mysql:host=localhost;dbname=cms
         public function conexion(){
             try{
-                $this->conex = new PDO($this->host_name, $this->user,$this->pass);
+                $this->conex = new PDO($this->dsn, $this->user,$this->pass);
                 echo "Conexion correcta";
 
             } catch(PDOException $e){
@@ -27,5 +27,5 @@
         public function getConexion(){
             return $this->conex; 
         }
+
     }
-//medida de seguridad: cualquier cosa andy lo explica si el profe pregunta...
