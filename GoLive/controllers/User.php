60308c3854df6a574072a3aa680eb5f84ae0a023
AnaCarbajo <aniic.1701@gmail.com>
2020-11-10 21:38:27 -0300

    .

--- GoLive/controllers/User.php
+++ GoLive/controllers/User.php
@@ -36,16 +36,16 @@ class UserController
            echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["firstName"]) ) ;
            echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["lastName"]) ) ;
            echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["username"]) ) ;
-           echo(preg_match('/^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])([0-9A-Za-z\d$@$!%?&]|[^ ]){8,15}$/', $_POST["email"]) ) ;
-           echo(preg_match('/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/', $_POST["password"]) ) ;
+           echo(preg_match('/^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/', $_POST["email"]) ) ;
+           echo(preg_match('/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,15}$/', $_POST["password"]) ) ;
            echo(preg_match('/^[1-3]{1,1}$/', $_POST["gender"]));
            
             if( 
                 preg_match('/^[a-zA-Z\s]+$/', $_POST["firstName"]) &&
                 preg_match('/^[a-zA-Z\s]+$/', $_POST["lastName"]) &&
                 preg_match('/^[a-zA-Z\s]+$/', $_POST["username"]) &&
-                preg_match('/^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])([0-9A-Za-z\d$@$!%?&]|[^ ]){8,15}$/', $_POST["email"]) &&
-                preg_match('/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/', $_POST["password"]) &&
+                preg_match('/^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/', $_POST["email"]) &&
+                preg_match('/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,15}$/', $_POST["password"]) &&
                 preg_match('/^[1-3]{1,1}$/', $_POST["gender"]))
             {
                 echo("ola");
