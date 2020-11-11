72144875dc67ac30ee6f612981bce7f17b6d2f97
andy10134 <andy10134@hotmail.com>
2020-11-10 21:48:55 -0300

    Expresiones regulares arregladas

--- GoLive/controllers/User.php
+++ GoLive/controllers/User.php
@@ -8,8 +8,8 @@ class UserController
     {
         if(isset($_POST["user"]) && isset($_POST["password"]))
         {
-            if(preg_match('/^[a-zA-Z\s]+$/', $_POST["user"]) &&
-            preg_match('/^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])([0-9A-Za-z\d$@$!%?&]|[^ ]){8,15}$/', $_POST["password"]))
+            if( preg_match('/^[a-zA-Z\s]+$/', $_POST["user"]) &&
+                preg_match('/^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])([0-9A-Za-z\d$@$!%?&]|[^ ]){8,15}$/', $_POST["password"]))
             {
                 $contraseña = crypt($_POST["password"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$'); //encriptamos la contraseña
                 $datosController = array("usuario"=>$_POST["user"],
@@ -33,9 +33,10 @@ class UserController
             isset($_POST["password"]) &&
             isset($_POST["gender"]))
         {
+            var_dump($_POST);
            echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["firstName"]) ) ;
            echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["lastName"]) ) ;
-           echo(preg_match('/^[a-zA-Z\s]+$/', $_POST["username"]) ) ;
+           echo(preg_match('/^[a-zA-Z0-9\s]+$/', $_POST["username"]) ) ;
            echo(preg_match('/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/', $_POST["email"]) ) ;
            echo(preg_match('/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,15}$/', $_POST["password"]) ) ;
            echo(preg_match('/^[1-3]{1,1}$/', $_POST["gender"]));
