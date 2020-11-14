e32f7d0152d9a9825b0bc9eb95268e845445f640
AnaCarbajo <aniic.1701@gmail.com>
2020-11-13 22:40:24 -0300

    .

--- GoLive/views/modules/login.php
+++ GoLive/views/modules/login.php
@@ -1,4 +1,3 @@
-
 <!-- ======= Header ======= -->
 <header id="header" class="fixed-top ">
     <div class="container-fluid">
@@ -7,7 +6,7 @@
         <div class="col-xl-9 d-flex align-items-center">
           <h1 class="logo mr-auto"><a href="index.html">GO LIVE</a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
-            <a href="index.html" class="logo mr-auto"><img src="assets/img/logo2.png" alt="" class="img-fluid"></a>-->
+            <a href="index.html" class="logo mr-auto"><img src="assets/img/logo2.png" alt="" class="img-fluid"></a>
 
           <nav class="nav-menu d-none d-lg-block">
             <ul>
@@ -44,46 +43,51 @@
 </header><!-- End Header -->
 
 <section id="hero" class="d-flex container-fluid justify-content-center align-items-center">
-	<div class="d-flex justify-content-center align-items-center" id="#form" style="height: 80vh;">
-		<div class="container col-10 row bg-primary d-flex justify-content-center align-items-center p-0 pb-lg-0 pb-md-5" style="">
-			<div class="info col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center " style="border-top-left-radius: 42px;border-bottom-left-radius: 42px;">
-				<p class="h3 pt-1 pb-3">
+    <div class="d-flex justify-content-center align-items-center" id="#form">
+        <div class="container row d-flex p-0 pb-lg-0 pb-md-5" style="background-color: #333a5aa3;">
+            <div class="col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center" style="color: #ffffff;">
+            <img src="assets/app/img/imgform.png" class="imgform">
+                <p class="h3 pt-1 pb-3">
 					¿Sos nuevo por acá? ;)
-				</p>
-				<p class="lead pb-3">
-					Comenza a manejar tus actividades de forma smart.
-				</p>
-				<p class="mt-2"><a class="boton" href="registrarse">
+                </p>
+                <p class="lead pb-3">
+					Comenza a manejar tus actividades de forma smart. <br> <strong>;)</strong>
+                </p>
+                <p class="mt-2"><a class="boton" href="registrarse">
 					Registrarse
-				</a></p>
-			</div>
+                    </a></p>
+            </div>
 
-			<div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center" style="border-top-right-radius: 42px; border-bottom-right-radius: 42px;">
+            <div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center" style="border-top-right-radius: 42px; border-bottom-right-radius: 42px;">
 
-			<h2 class="h2">
+                <h2 style="color:#0B0050; font-size: 35px;">
 					Iniciar Sesion
-			</h2>
+                </h2>
 
-				<form method="POST" action="">
+                <form METHOD="POST" action="">
 				<div class="form-group row d-flex flex-column align-items-center">
 
 					<input class="form-control mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 col-xs-12" type="text" name="username" placeholder="Usuario">
 
 					<input class="form-control  mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 col-xs-12" type="password" name="password" placeholder="Contraseña">
 
-					<button type="submit" class=" boton rounded-pill mt-5 col-lg-4 col-md-6 col-xs-8 ">Registrarse</button>
+					<button type="submit" class=" boton rounded-pill mt-5 col-md-6 col-xs-8 ">Ingresar</button>
 
 				</div>
+                </form>
+            </div>
 
-				</form>
-
-			</div>
-
-		</div>
-	</div>
+        </div>
+    </div>
 </section>
 
+
 <style>
+
+.imgform{
+  height: 165px;
+  margin-bottom: 22px;
+}
 
 .container{
 	font-family: 'Poppins', sans-serif;
@@ -95,12 +99,8 @@
 	font-family: sans-serif;
 }
 
-h2.h2{
-	color: #0B0050;
-}
 
 .info{
-	color : #f9f8ff;
 	height: 100%;
 	background: linear-gradient(90deg, rgba(11,0,80,1) 0%, rgba(7,4,41,1) 100%);
 }
@@ -126,8 +126,9 @@ p>a.boton:hover{
 }
 
 .form{
-	background-color: #fff;
-	height: 100%;
+  background-color: rgb(255 253 253);
+  height: 70vh;
+  width: 50%;
 }
 
 form input{
@@ -143,5 +144,5 @@ form .boton:hover{
 	background-color: #fff;
 	color:  rgb(11,0,80);
 	border: rgb(11,0,80) 1.5px solid;
-}   
+}
 </style>
