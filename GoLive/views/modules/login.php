a3ba853e81613665b3a13614e9ae7c4681f2c7aa
pleitons <agustingall12@gmail.com>
2020-11-16 04:48:59 -0300

    Cambio en estilos

--- GoLive/views/modules/login.php
+++ GoLive/views/modules/login.php
@@ -1,5 +1,5 @@
 <!-- ======= Header ======= -->
-<header id="header" class="fixed-top ">
+<header id="header" class="fixed-top">
     <div class="container-fluid">
 
       <div class="row justify-content-center">
@@ -42,105 +42,205 @@
 	</div>
 </header><!-- End Header -->
 
-<section id="hero" class="d-flex container-fluid justify-content-center align-items-center">
-    <div class="d-flex justify-content-center align-items-center" id="#form" style="height: 80vh; width: 100%;">
-        <div class="container col-lg-8 col-md-12 row d-flex justify-content-center align-items-center p-0 pb-lg-0 pb-md-5" style="background-color: #333a5aa3;">
-            <div class="col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center" style="color: #ffffff;">
-            <img src="assets/app/img/34839d0e5658c919930ed9fd3c8b1437.png" class="imgform">
-                <p class="h3 pt-1 pb-3">
-					¿Sos nuevo por acá?
-                </p>
-                <p class="lead pb-3">
-					Comenza a manejar tus actividades de forma smart. <br> <strong>;)</strong>
-                </p>
-                <p class="mt-2"><a class="boton" href="registrarse">
-					Registrarse
-                    </a></p>
-            </div>
+<main class="row align-items-center justify-content-center m-0">
+  
 
-            <div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center" style="border-top-right-radius: 42px; border-bottom-right-radius: 42px;">
-
-                <h2 style="color:#0B0050; font-size: 35px; margin-bottom:20px;">
-					Iniciar Sesion
-                </h2>
-
-                <form METHOD="POST" action="">
-				<div class="form-group row d-flex flex-column align-items-center">
-
-					<input class="form-control mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-10 col-md-10 col-xs-12" type="text" name="username" placeholder="Usuario">
-
-					<input class="form-control  mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-10 col-md-10 col-xs-12" type="password" name="password" placeholder="Contraseña">
-
-					<button type="submit" class=" boton rounded-pill mt-5 col-md-6 col-xs-8 ">Ingresar</button>
-
-				</div>
-                </form>
-            </div>
-        </div>
+  <section class="container col-lg-10 col-xs-11 row d-flex p-0">
+    <div class="info col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center ">
+      <img src="assets/app/img/34839d0e5658c919930ed9fd3c8b1437.png" class="imgform">
+      <p class="h3 pt-1 pb-3">
+        ¿Sos nuevo por acá?
+      </p>
+      <p class="lead pb-3">
+        Comenza a manejar tus actividades de forma smart. ;)
+      </p>
+      <a class="boton">
+        Registrarse
+      </a>
     </div>
-</section>
+
+    <div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center">
+
+    <h2 class="h2 p-0 m-0">
+        Iniciar Sesión
+    </h2>
+
+       <form METHOD="POST" action="">
+                <div class="form-group row d-flex flex-column align-items-center">
+              
+                    <input class="form-control mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 " type="email" name="email" placeholder="E-mail">
+                    <input class="form-control  mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 " type="password" name="password" placeholder="Contraseña">
+ 
+                    
+                    <button type="submit" class=" boton rounded-pill mt-5 col-lg-4 col-md-6  ">Entrar</button>
+                </div>
+            </form>    
+    </div>
+
+  </section>
+
+  </main>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
 
 
 <style>
+@media (min-width: 993px) { 
+
+  main{
+    height: 60vh;
+  }
+
+
+
+
+  #header{
+    position: unset;
+  }
+
+ }
+
+ @media (min-height: 700px ) and (min-width: 370px) and (max-height: 737px) {
+  body{
+    padding-bottom: 27vw;
+  }
+ }
+
+ @media (min-height: 738px ) and (min-width: 370px) and (max-height: 840px) {
+  body{
+    padding-bottom: 48vw;
+  }
+ }
+
+
+@media (max-height: 1199px) { 
+ }
+
+
+
+@media (max-width: 992px ) and (min-width: 577px) {
+
+  .info{
+    border-radius: 40px 40px 0 0 !important;
+  }
+  .form{
+    border-radius: 0 0 40px 40px  !important;
+  }
+
+}
+
+@media (max-width: 767.98px ) {
+
+  .form{
+    padding: 12% !important;
+  }
+  .info{
+    padding: 12% !important;
+  }
+  .container{
+    margin: 5% 0 ;
+  }
+ }
+
+ @media (max-width: 576px) { 
+
+  .form{
+    pading: 15% !important;
+  }
+
+  .form, .info{
+    border-radius: 0 !important;
+  }
+
+  .container{
+    padding: 0;
+    margin:0;
+  }
+
+ 
+  }
+
+
+
+
+body{
+  width: 100%;
+  background: linear-gradient(90deg, rgba(4,178,115,1) 0%, rgba(5,126,93,1) 18%, rgba(6,77,72,1) 30%, rgba(7,4,41,1) 62%,rgba(7,4,41,1) 100%);
+}
+
+.container{
+  font-family: 'Poppins', sans-serif; 
+}
+
+.form{
+  padding: 11% 5%;
+  border-radius: 0 40px 40px 0;
+}
 
 .imgform{
   height: 165px;
   margin-bottom: 22px;
 }
 
-.container{
-	font-family: 'Poppins', sans-serif;
-	height: 90%;
-	border-radius: 40px;
-}
-
 .lead{
-	font-family: sans-serif;
+  font-family: sans-serif;
 }
 
+h2.h2{
+  color: #0B0050;
+}
 
 .info{
-	height: 100%;
-	background: linear-gradient(90deg, rgba(11,0,80,1) 0%, rgba(7,4,41,1) 100%);
+  padding: 5% ;
+  color : #f9f8ff;
+  background: #333a5aa3;
+  border-radius: 40px 0 0 40px;
 }
 
 .boton{
-	color: #fff;
-	width: 50%;
-	padding: 15px 60px;
-	border: 2px solid #fff;
-	border-radius: 30px;
-	letter-spacing: 2px;
-	transition: background .1s linear .1s;
-}
-p>a.boton:hover{
-	text-decoration: none;
-	color: rgb(11,0,80);
-	background-color: #fff;
-	cursor: pointer;
+  color: #fff;
+
+  
+  border: 2px solid #fff;
+  border-radius: 30px;
+  letter-spacing: 2px;
+  transition: background .1s linear .1s;
 }
 
-.container div{
-	padding: 0 4vw;
+a.boton {
+  padding: 15px 60px;
+}
+
+.form .boton {
+  padding: 15px 60px;
+}
+
+a.boton:hover{
+  text-decoration: none;
+  color: rgb(11,0,80);
+  background-color: #fff;
+  cursor: pointer;
 }
 
 .form{
-  background-color: rgb(255 253 253);
-  height: 100%;
+  background-color: #fff;
 }
 
 form input{
-	height: 15px;
+  height: 15px;
 }
 
 form .boton{
-	background-color: rgb(11,0,80);
-	color: #fff;
+  background-color: rgb(11,0,80);
+  color: #fff;
 }
 
 form .boton:hover{
-	background-color: #fff;
-	color:  rgb(11,0,80);
-	border: rgb(11,0,80) 1.5px solid;
+  background-color: #fff;
+  color:  rgb(11,0,80);
+  border: rgb(11,0,80) 1.5px solid;
 }
 </style>
--- GoLive/views/modules/signup.php
+++ GoLive/views/modules/signup.php
@@ -1,5 +1,5 @@
 <!-- ======= Header ======= -->
-<header id="header" class="fixed-top ">
+<header id="header" class="fixed-top">
     <div class="container-fluid">
 
       <div class="row justify-content-center">
@@ -42,30 +42,31 @@
 	</div>
 </header><!-- End Header -->
 
-<section id="hero" class="d-flex container-fluid justify-content-center align-items-center">
-    <div class="d-flex justify-content-center align-items-center" id="#form">
-        <div class="container col-10 row d-flex justify-content-center align-items-center p-0 pb-lg-0 pb-md-5" style="background-color: #333a5aa3;">
-            <div class="col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center" style="color: #ffffff;">
-            <img src="assets/app/img/imgform.png" class="imgform">
-                <p class="h3 pt-1 pb-3">
-                    ¿Ya tenes cuenta?
-                </p>
-                <p class="lead pb-3">
-                    Si ese ese es el caso podés iniciar sesíon acá <br> <strong>;)</strong>
-                </p>
-                <p class="mt-2"><a class="boton" href="ingresar">
-                        Iniciar Sesion
-                    </a></p>
-            </div>
+<main class="row align-items-center justify-content-center m-0">
+  
 
-            <div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center" style="border-top-right-radius: 42px; border-bottom-right-radius: 42px;">
+  <section class="container col-lg-10 col-xs-11 row d-flex p-0">
+    <div class="info col-lg-4 col-md-12 text-center d-flex flex-column justify-content-center ">
+      <img src="assets/app/img/34839d0e5658c919930ed9fd3c8b1437.png" class="imgform">
+      <p class="h3 pt-1 pb-3">
+       ¿Ya tenes cuenta?
+      </p>
+      <p class="lead pb-3">
+        Si ese ese es el caso podés iniciar sesíon acá <br> <strong>;)</strong>
+      </p>
+      <a class="boton">
+        Iniciar Sesion
+      </a>
+    </div>
 
-                <h2 style="color:#0B0050; font-size: 35px;">
-                    Crear Cuenta
-                </h2>
+    <div class="form col-lg-8 col-md-12 d-flex flex-column justify-content-center text-center">
 
-                <form METHOD="POST" action="">
-                    <div class="form-group row d-flex flex-column align-items-center">
+    <h2 class="h2 p-0 m-0">
+        Registrarse
+    </h2>
+
+       <form METHOD="POST" action="">
+               <div class="form-group row d-flex flex-column align-items-center">
 
                         <input class="form-control mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 col-xs-12" type="text" name="firstName" placeholder="Nombre" id="name">
                         <input class="form-control mt-3 pt-3 pb-3 pl-4 pr-4 col-lg-8 col-md-10 col-xs-12" type="text" name="lastName" placeholder="Apellido">
@@ -104,75 +105,167 @@
                         </fieldset>
                         <button type="submit" class=" boton rounded-pill mt-2 col-md-6 col-xs-8 ">Registrarse</button>
                     </div>
-
-                </form>
-
-            </div>
-
-        </div>
+            </form>    
     </div>
-</section>
+
+  </section>
+
+  </main>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
 
 
 <style>
+@media (min-width: 993px) { 
+
+  main{
+    height: 60vh;
+  }
+
+  #header{
+  	position: unset;
+  }
+
+
+
+
+ }
+
+ @media (min-height: 700px ) and (min-width: 370px) and (max-height: 737px) {
+  body{
+    padding-bottom: 27vw;
+  }
+ }
+
+ @media (min-height: 738px ) and (min-width: 370px) and (max-height: 840px) {
+  body{
+    padding-bottom: 48vw;
+  }
+ }
+
+
+@media (max-height: 1199px) { 
+ }
+
+
+
+@media (max-width: 992px ) and (min-width: 577px) {
+
+  .info{
+    border-radius: 40px 40px 0 0 !important;
+  }
+  .form{
+    border-radius: 0 0 40px 40px  !important;
+  }
+
+}
+
+@media (max-width: 767.98px ) {
+
+  .form{
+    padding: 12% !important;
+  }
+  .info{
+    padding: 12% !important;
+  }
+  .container{
+    margin: 5% 0 ;
+  }
+ }
+
+ @media (max-width: 576px) { 
+
+  .form{
+    pading: 15% !important;
+  }
+
+  .form, .info{
+    border-radius: 0 !important;
+  }
+
+  .container{
+    padding: 0;
+    margin:0;
+  }
+}
+
+body{
+  width: 100%;
+  background: linear-gradient(90deg, rgba(4,178,115,1) 0%, rgba(5,126,93,1) 18%, rgba(6,77,72,1) 30%, rgba(7,4,41,1) 62%,rgba(7,4,41,1) 100%);
+}
+
+.container{
+  font-family: 'Poppins', sans-serif; 
+}
+
+.form{
+  padding: 5% 5% 4% 5%;
+  border-radius: 0 40px 40px 0;
+  background-color: #fff;
+}
 
 .imgform{
   height: 165px;
   margin-bottom: 22px;
 }
 
-.container{
-	font-family: 'Poppins', sans-serif;
-	height: 90%;
-	border-radius: 40px;
-}
-
 .lead{
-	font-family: sans-serif;
+  font-family: sans-serif;
 }
 
+h2.h2{
+  color: #0B0050;
+}
 
 .info{
-	height: 100%;
-	background: linear-gradient(90deg, rgba(11,0,80,1) 0%, rgba(7,4,41,1) 100%);
+  padding: 5% ;
+  color : #f9f8ff;
+  background: #333a5aa3;
+  border-radius: 40px 0 0 40px;
 }
 
 .boton{
-	color: #fff;
-	width: 50%;
-	padding: 15px 60px;
-	border: 2px solid #fff;
-	border-radius: 30px;
-	letter-spacing: 2px;
-	transition: background .1s linear .1s;
-}
-p>a.boton:hover{
-	text-decoration: none;
-	color: rgb(11,0,80);
-	background-color: #fff;
-	cursor: pointer;
+  color: #fff;
+
+  
+  border: 2px solid #fff;
+  border-radius: 30px;
+  letter-spacing: 2px;
+  transition: background .1s linear .1s;
 }
 
-.container div{
-	padding: 0 4vw;
+a.boton {
+  padding: 15px 60px;
 }
 
-.form{
-  background-color: rgb(255 253 253);
+.form .boton {
+  padding: 15px 60px;
 }
+
+a.boton:hover{
+  text-decoration: none;
+  color: rgb(11,0,80);
+  background-color: #fff;
+  cursor: pointer;
+}
+
+
 
 form input{
-	height: 15px;
+  height: 15px;
 }
 
 form .boton{
-	background-color: rgb(11,0,80);
-	color: #fff;
+  background-color: rgb(11,0,80);
+  color: #fff;
 }
 
 form .boton:hover{
-	background-color: #fff;
-	color:  rgb(11,0,80);
-	border: rgb(11,0,80) 1.5px solid;
+  background-color: #fff;
+  color:  rgb(11,0,80);
+  border: rgb(11,0,80) 1.5px solid;
 }
 </style>
